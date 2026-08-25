4be14e67181a57063c5ff43b4310602c3be42e1e
Alaa <alaamadi005@gmail.com>
2026-08-25 17:34:23 +0300

    feat(github-client): add index.ts

---
+++ packages/github-client/src/index.ts
@@ -0,0 +1,9 @@
+export { GitHubClient, type GitHubClientOptions } from "./client";
+export { GitHubClientError, type GitHubClientErrorKind } from "./errors";
+export type {
+  GitHubActor,
+  GitHubIssue,
+  GitHubQuota,
+  GitHubRepository,
+  GitHubResponse,
+} from "./types";
