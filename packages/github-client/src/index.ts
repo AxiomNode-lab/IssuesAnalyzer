208c6e017c4eaed77cc2ea5bd9513c7227cacd52
Alaa <alaamadi005@gmail.com>
2026-08-26 11:09:52 +0300

    feat(github-client): export activity evidence models

--- packages/github-client/src/index.ts
+++ packages/github-client/src/index.ts
@@ -2,9 +2,12 @@ export { GitHubClient, type GitHubClientOptions } from "./client";
 export { GitHubClientError, type GitHubClientErrorKind } from "./errors";
 export type {
   GitHubActor,
+  GitHubCommitEvidence,
+  GitHubCommunityProfile,
   GitHubIssue,
   GitHubIssueComment,
   GitHubQuota,
+  GitHubReleaseEvidence,
   GitHubRepository,
   GitHubResponse,
 } from "./types";
